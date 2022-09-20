6fd7c83026860066e05e7f65681c1aa1e8360653
Rohan Bhardwaj <94526837+rohanbhardwaj205@users.noreply.github.com>
2022-09-21 01:00:19 +0530

    Create Sorting program.cpp

---
+++ Sorting program.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool check_if_sorted(int arr[],int n)
+{
+    for(int i=1;i<n;i++)
+    {
+        if(arr[i]<arr[i-1])
+        return false;//why return ??
+    }
+    return true;//why didn't else come here??
+    int main()
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    if (check_if_sorted(arr,n)==false)
+    {
+        cout<<"Not sorted";
+    }
+    else
+    {
+        cout<<"Sorted";
+    }
+    return 0;
+}
