c579ce2f7789a4c32886101b0f96b0b1e4bb094d
Rohan Bhardwaj <94526837+rohanbhardwaj205@users.noreply.github.com>
2022-09-21 01:01:07 +0530

    Update Sorting program.cpp

--- Sorting program.cpp
+++ Sorting program.cpp
@@ -5,10 +5,13 @@ bool check_if_sorted(int arr[],int n)
     for(int i=1;i<n;i++)
     {
         if(arr[i]<arr[i-1])
-        return false;//why return ??
+            return false; //why return ??
+        else 
+            return true; //why didn't else come here??
     }
-    return true;//why didn't else come here??
-    int main()
+}
+int main()
+{
     int n;
     cin>>n;
     int arr[n];
